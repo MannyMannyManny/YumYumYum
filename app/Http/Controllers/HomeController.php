a00be3ec1ec367c76b805c6117af9c30b852ac99
Kirill Mikhailov <dfgrddfgrd@yandex.ru>
2016-03-09 02:23:08 +0300

    Dynamic API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Carbon;
 use Response;
 use App\Shares;
+use App\Settings;
 use View;
 use App\Quotation;
 use App\Http\Requests;
@@ -14,8 +15,10 @@ class HomeController extends Controller
 {
     public function indexPage ()
     {
+        $fbkey = Settings::where('key', 'facebookid')->first();
         return View::make('index', array(
             "fb" => Shares::where('type', '=', 'fb')->count(), 
+            "fbapi" => $fbkey['value'],
             "tw" => Shares::where('type', '=', 'tw')->count(),
             "ln" => Shares::where('type', '=', 'ln')->count(),
             "gp" => Shares::where('type', '=', 'gp')->count()
